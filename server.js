42450dda92da8a490b1e079f05a7ea8b8e4abdd3
Ashwin <8831182+ashwinr63@users.noreply.github.com>
2022-03-12 00:35:44 -0500

    adding new config

--- server.js
+++ server.js
@@ -12,8 +12,7 @@ const db = knex({
   // connect to your own database here:
   client: 'pg',
   connection: {
-    connectionString : process.env.DATABASE_URL,
-    ssl: true,
+    connectionString : process.env.DATABASE_URL
   }
 });
 
