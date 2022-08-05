1437aba692dde55705292a1833d970d45dcad91d
Ashwin <8831182+ashwinr63@users.noreply.github.com>
2022-08-05 00:24:09 -0400

    Adding conectionstring to Pg

--- server.js
+++ server.js
@@ -20,9 +20,8 @@ const db = knex (
         }
     }
 )
-
+const app = express();
 app.use(express.json())
-app.use(bodyParser.json())
 app.use(cors())
 
 app.get('/', (req, res)=> { res.send('it is working') })
